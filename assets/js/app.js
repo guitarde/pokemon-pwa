dfdd8535b9547a554b910a6e4915c6872fccb5d0
tomascavataio <tomascavataio@gmail.com>
2020-07-02 19:37:49 +0200

    Add pokeball buttons, modify styles, fix serviceWorker

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 const pokemonBase = "https://pokeapi.co/api/v2/pokemon-form";
-const image = document.querySelector(".image");
+const pokemonImage = document.querySelector(".pokemon-image");
 const name = document.querySelector(".name");
+
+const pokeball = document.querySelector(".pokeball");
+const openPokeball = document.querySelector(".open-pokeball")
 
 /**
  *  Fetches a pokemon through pókeAPI.
@@ -9,26 +12,34 @@ async function getData() {
     const randomPokemon = Math.floor(Math.random() * 150);
 
     //TODO - Mostrar bola
+    setTimeout(function () {
+        pokemonImage.style.display = "block",
+            openPokeball.style.display = "none"
+    }, 1000);
+
     return await fetch(`${pokemonBase}/${randomPokemon}/`)
         .then((response) => response.json())
         .then((data) => {
-            image.src = data.sprites.front_default;
+            pokemonImage.src = data.sprites.front_default;
             name.innerText = data.name;
-            //TODO-ocultar bola
+            pokemonImage.style.display = "none"
+            openPokeball.style.display = "block"
         })
         .catch((error) => {
             console.error("Cannot get data", error);
         });
+
 }
 
 getData();
 
-document.querySelector(".another-one-btn").addEventListener("click", getData);
+document.querySelector(".pokeball").addEventListener("click", getData);
+
 
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("./assets/js/serviceWorker.js")
-        .then(function() {
+        .register("./serviceWorker.js")
+        .then(function () {
             console.log("Service Worker Registered");
         });
 }
